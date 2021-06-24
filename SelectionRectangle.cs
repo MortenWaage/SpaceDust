dba2ceecfc9ccca4ea86d74ea385ee64b6a49191
MortenWaage <84008835+MortenWaage@users.noreply.github.com>
2021-06-24 15:29:18 +0200

    Add files via upload

---
+++ SelectionRectangle.cs
@@ -0,0 +1,251 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionRectangle : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] RectTransform rectSelectSpriteGreen;
+    [SerializeField] RectTransform rectSelectSpriteRed;
+
+    private Vector3 dragCheck;
+    private Vector3 rectStart;
+    private Vector3 rectEnd;
+
+    private bool isDragging = false;
+    bool enemyDrag = false;
+
+    private int startDragDistance = 3;
+
+    private void Start()
+    {
+        rectSelectSpriteGreen.gameObject.SetActive(false);
+        rectSelectSpriteRed.gameObject.SetActive(false);
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("SelectEnemy"))
+        {
+            enemyDrag = true;
+        }
+        else
+        {
+            enemyDrag = false;
+        }
+        
+
+        switch (enemyDrag)
+        {
+            case false:
+
+                GreenSelectionRectangleLoop();
+
+                break;
+
+            case true:
+
+                RedSelectionRectangleLoop();
+
+                break;
+
+        }      
+    }
+
+
+    private void GreenSelectionRectangleLoop()
+    {
+        switch (isDragging)
+        {
+            case true:
+
+                if (!Input.GetMouseButton(0)) ChangeGreenState(false);
+
+
+                DrawGreenRectangle();
+
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    SetSelectionScreenRect();
+                    ChangeGreenState(false);
+                    ChangeRedState(false);
+                    enemyDrag = false;
+                }
+
+                break;
+
+            case false:
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    dragCheck = Input.mousePosition;
+                    if (dragCheck == Vector3.zero) break;
+
+                    ChangeGreenState(true);
+                    SetRectStartPosition();
+                }
+
+                break;
+        }
+    }
+    private void RedSelectionRectangleLoop()
+    {
+        switch (isDragging)
+        {
+            case true:
+
+                if (!Input.GetMouseButton(0)) ChangeRedState(false);
+
+                                
+                DrawRedRectangle();
+
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    SetSelectionScreenRect();                    
+                    ChangeGreenState(false);
+                    ChangeRedState(false);
+                    enemyDrag = false;
+                }
+
+                break;
+
+            case false:
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    dragCheck = Input.mousePosition;
+                    if (dragCheck == Vector3.zero) break;
+
+                    ChangeRedState(true);
+                    SetRectStartPosition();
+                }
+
+                break;
+        }
+    }
+    private void DrawGreenRectangle()
+    {
+        if (!rectSelectSpriteGreen.gameObject.activeInHierarchy)
+            rectSelectSpriteGreen.gameObject.SetActive(true);
+
+        rectEnd = Input.mousePosition;
+        Vector3 center = (rectStart + rectEnd) * 0.5f;
+        rectSelectSpriteGreen.position = center;
+
+        float sizeX = Mathf.Abs(rectStart.x - rectEnd.x);
+        float sizeY = Mathf.Abs(rectStart.y - rectEnd.y);
+
+        rectSelectSpriteGreen.sizeDelta = new Vector2(sizeX, sizeY);
+    }
+    private void DrawRedRectangle()
+    {
+        if (!rectSelectSpriteRed.gameObject.activeInHierarchy)
+            rectSelectSpriteRed.gameObject.SetActive(true);
+
+        rectEnd = Input.mousePosition;
+        Vector3 center = (rectStart + rectEnd) * 0.5f;
+        rectSelectSpriteRed.position = center;
+
+        float sizeX = Mathf.Abs(rectStart.x - rectEnd.x);
+        float sizeY = Mathf.Abs(rectStart.y - rectEnd.y);
+
+        rectSelectSpriteRed.sizeDelta = new Vector2(sizeX, sizeY);
+    }
+    private void SetSelectionScreenRect()
+    {
+
+        Vector3 center = (rectStart + rectEnd) * 0.5f;
+        float rectHalfWidth = Mathf.Abs(rectStart.x - rectEnd.x) * 0.5f;
+        float rectHalfHeight = Mathf.Abs(rectStart.y - rectEnd.y) * 0.5f;
+
+        Rect rect = new Rect(center.x - rectHalfWidth, center.y - rectHalfHeight, (rectHalfWidth * 2), (rectHalfHeight * 2));
+
+        if (!enemyDrag) CheckFriendlyUnitsInSelection(rect);
+        else CheckEnemyUnitsInSelection(rect);
+
+    }
+    void CheckFriendlyUnitsInSelection(Rect rect)
+    {
+        var playerShips = MainController.Instance.unitManager.PlayerShips;
+
+        foreach (var unit in playerShips)
+        {
+            ShipCore ship = unit.GetComponent<ShipCore>();
+            Vector3 shipScreenPosition = ship.IsInView();
+
+            if (shipScreenPosition == Vector3.zero || ship.IsDead()) continue;
+
+            if (rect.Contains(new Vector2(shipScreenPosition.x, shipScreenPosition.y)))
+                ship.AppendToSelectedList();
+   
+        }
+    }
+    void CheckEnemyUnitsInSelection(Rect rect)
+    {      
+        List<GameObject> listOfEnemies = GetEnemiesInSelection(rect);
+
+        var selectedShips = MainController.Instance.unitController.SelectedShips;
+        if (selectedShips == null) return;
+
+        foreach (GameObject ship in selectedShips)
+        {
+            if (ship == null) continue;
+
+            var core = ship.GetComponent<ShipCore>();
+            core.ClearAllTargets();
+            core.NavClearDestinationList();
+
+            core.SetNewTargetList(listOfEnemies);
+
+            core.NavSetAutomaticControl();
+            core.NavSetCombatStateAttack();
+            core.SetCombatStatePursue();
+            
+        }
+    }
+    List<GameObject> GetEnemiesInSelection(Rect rect)
+    {
+        var enemyShips = MainController.Instance.unitManager.EnemyShips;
+        List<GameObject> listOfTargets = new List<GameObject>();
+
+        foreach (var unit in enemyShips)
+        {
+            ShipCore ship = unit.GetComponent<ShipCore>();
+            Vector3 shipScreenPosition = ship.IsInView();
+
+            if (shipScreenPosition == Vector3.zero || ship.IsDead()) continue;
+
+            if (rect.Contains(new Vector2(shipScreenPosition.x, shipScreenPosition.y)))
+            {
+                listOfTargets.Add(unit);
+            }
+        }
+
+        return listOfTargets;
+    }
+
+    private void SetRectStartPosition()
+    {
+        if (Vector3.Distance(dragCheck, Input.mousePosition) > startDragDistance)
+        {
+            rectStart = Input.mousePosition;
+            rectEnd = rectStart;
+        }
+    }
+    private void ChangeGreenState(bool newState)
+    {
+        isDragging = newState;
+        rectSelectSpriteGreen.gameObject.SetActive(newState);
+        dragCheck = Vector3.zero;
+    }
+    private void ChangeRedState(bool newState)
+    {
+        isDragging = newState;
+        rectSelectSpriteRed.gameObject.SetActive(newState);
+        dragCheck = Vector3.zero;
+    }
+}
